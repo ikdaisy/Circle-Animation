28bfac8647237b56a1d441808503841c185731b8
safa <safakallianthodi6@gmail.com>
2024-08-20 10:15:30 +0530

    Added

---
+++ js/disc.js
@@ -0,0 +1,11 @@
+document.getElementById("disc").addEventListener("mousemove",(event)=>{
+    x=event.clientX;
+    // console.log(x);
+    y=event.clientY;
+    // console.log(y);
+    document.getElementById("disc").style.left=x+"px"
+    document.getElementById("disc").style.top=y+"px"
+})
+   
+    
+    
